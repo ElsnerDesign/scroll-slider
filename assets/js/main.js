2c4551faab3d305a4d6e3ef47b69b6a75972caee
Elsner Design <khyati@xhtmljunkies.com>
2021-03-02 19:08:35 +0530

    add files

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    (
+        $(".o-horizontal-slider").length && $(".o-horizontal-slider").mCustomScrollbar({ axis: "x", mouseWheel: { enable: !1 } }),
+        $("#video").length)
+});
